8668328eb4b41abb62ee5832229094a39b541c67
ShocKwav3_ <ahmonemon@gmail.com>
2020-03-08 16:25:35 +0200

    device data endpoint for filter options added

--- src/routes/api/v1/deviceData/deviceDataCollection.js
+++ src/routes/api/v1/deviceData/deviceDataCollection.js
@@ -6,6 +6,8 @@ import controllers from 'projectRoot/src/controllers';
 let router = express.Router();
 
 router.get('/', controllers.deviceDataController.getAllDeviceData);
+router.get('/role/:roleId', controllers.deviceDataController.getDeviceDataByRole);
+router.get('/category/:categoryId', controllers.deviceDataController.getDeviceDataByCategory);
 
 router.post('/', controllers.deviceDataController.addDeviceData);
 
