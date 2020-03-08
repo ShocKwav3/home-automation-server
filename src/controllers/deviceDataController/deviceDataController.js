595c91d313a3f62f8ee65e1fc038b4d84e6d966e
ShocKwav3_ <ahmonemon@gmail.com>
2020-03-08 16:25:02 +0200

    filter options based on category and role for device data endpoint

--- src/controllers/deviceDataController/deviceDataController.js
+++ src/controllers/deviceDataController/deviceDataController.js
@@ -1,56 +1,135 @@
-import model from 'projectRoot/src/models';
-import helpers from 'projectRoot/src/helpers';
-import { controllerConstants } from 'projectRoot/src/config/constants';
+import { Op } from 'sequelize';
 
-const { device_data } = model;
-const contextName = controllerConstants.deviceData.CONTEXTNAME
-const socketEventName = controllerConstants.deviceData.SOCKETEVENT
+import model from 'src/models';
+import helpers from 'src/helpers';
+import { controllerConstants } from 'src/config/constants';
+
+
+const {
+    device_data,
+    device,
+    category,
+    role
+} = model;
+const contextName = controllerConstants.deviceData.CONTEXTNAME;
+const socketEventName = controllerConstants.deviceData.SOCKETEVENT;
 
 const addDeviceData = (req, res) => {
-  const {
-    created_timestamp,
-    device_id,
-    device_value,
-    initiator_id,
-    initiator_role_id,
-  } = req.body
+    const {
+        created_timestamp,
+        device_id,
+        device_value,
+        initiator_id,
+        initiator_role_id,
+    } = req.body;
 
-  const deviceData = {
-    created_timestamp,
-    device_id,
-    device_value,
-    initiator_id,
-    initiator_role_id,
-  }
+    const deviceData = {
+        created_timestamp,
+        device_id,
+        device_value,
+        initiator_id,
+        initiator_role_id,
+    };
 
-  const cacheClient = req.app.get('cacheClient')
-  const cacheKey = res.locals.cacheKey
-  const shouldFireSocketEvent = true
+  const cacheClient = req.app.get('cacheClient');
+  const cacheKey = res.locals.cacheKey;
+  const shouldFireSocketEvent = true;
 
   return device_data.create(deviceData)
-                          .then(deviceDataSynced =>
-                            helpers.controllerHelpers.afterCreateSuccess(res, deviceDataSynced, contextName, cacheClient, cacheKey, shouldFireSocketEvent, socketEventName)
-                          ).catch(error =>
-                            res.status(401)
-                            .send(helpers.responseHelpers.addFailure(contextName, error))
-                          );
+                    .then(deviceDataSynced =>
+                        helpers.controllerHelpers.afterCreateSuccess(res, deviceDataSynced, contextName, cacheClient, cacheKey, shouldFireSocketEvent, socketEventName)
+                    ).catch(error =>
+                        res.status(401)
+                           .send(helpers.responseHelpers.addFailure(contextName, error))
+                    );
 };
 
 const getAllDeviceData = (req, res) => {
-  const cacheClient = req.app.get('cacheClient')
-  const cacheKey = res.locals.cacheKey
+  const cacheClient = req.app.get('cacheClient');
+  const cacheKey = res.locals.cacheKey;
 
   return device_data.findAll()
-                          .then(allDeviceData =>
-                            helpers.controllerHelpers.afterFetchSuccess(res, allDeviceData, contextName, cacheClient, cacheKey)
-                          ).catch(error =>
-                            res.status(400)
-                            .send(helpers.responseHelpers.fetchFailure(contextName, error))
-                          );
+                    .then(allDeviceData =>
+                        helpers.controllerHelpers.afterFetchSuccess(res, allDeviceData, contextName, cacheClient, cacheKey)
+                    ).catch(error =>
+                        res.status(400)
+                           .send(helpers.responseHelpers.fetchFailure(contextName, error))
+                    );
+};
+
+const getDeviceDataByRole = (req, res) => {
+    const cacheClient = req.app.get('cacheClient');
+    const cacheKey = res.locals.cacheKey;
+
+    const roleId = req.params.roleId;
+    const query = {
+        include: [{
+            model: device,
+            attributes: ['id'],
+            include: [{
+                model: category,
+                attributes: ['id'],
+                include: [{
+                    model: role,
+                    attributes: ['id'],
+                    where: {
+                        id: {
+                            [Op.eq]: roleId,
+                        },
+                    },
+                    required: true,
+                }],
+                required: true,
+            }],
+            required: true,
+        }],
+    };
+
+    return device_data.findAll(query)
+                      .then(allDeviceDataByRole =>
+                          helpers.controllerHelpers.afterFetchSuccess(res, allDeviceDataByRole, contextName, cacheClient, cacheKey)
+                      ).catch(error =>
+                          res.status(400)
+                             .send(helpers.responseHelpers.fetchFailure(contextName, error))
+                      );
+};
+
+const getDeviceDataByCategory = (req, res) => {
+    const cacheClient = req.app.get('cacheClient');
+    const cacheKey = res.locals.cacheKey;
+
+    const categoryId = req.params.categoryId;
+    const query = {
+        include: [{
+            model: device,
+            attributes: ['id'],
+            include: [{
+                model: category,
+                attributes: ['id'],
+                where: {
+                    id: {
+                        [Op.eq]: categoryId,
+                    },
+                },
+                required: true,
+            }],
+            required: true,
+        }],
+    };
+    
+    return device_data.findAll(query)
+                      .then(allDeviceDataByRole =>
+                          helpers.controllerHelpers.afterFetchSuccess(res, allDeviceDataByRole, contextName, cacheClient, cacheKey)
+                      ).catch(error =>
+                          res.status(400)
+                             .send(helpers.responseHelpers.fetchFailure(contextName, error))
+                      );
 };
 
 
 export default {
   addDeviceData,
   getAllDeviceData,
+  getDeviceDataByRole,
+  getDeviceDataByCategory,
 };
