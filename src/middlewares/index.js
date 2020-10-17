e443594775b6dde5a01c023fe5dda6274e832419
ShocKwav3_ <ahmonemon@gmail.com>
2020-10-18 02:23:24 +0300

    added https

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,8 +1,10 @@
 import cacheMiddlewares from './cacheMiddlewares';
 import tokenMiddlewares from './tokenMiddlewares';
+import forceHttpsMiddleware from './forceHttpsMiddleware';
 
 
 export default {
+    forceHttpsMiddleware,
     cacheMiddlewares,
     tokenMiddlewares,
 }
